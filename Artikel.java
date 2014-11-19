26891b02f0fb9cb3ebfc7736f6f9c10a49e18b3c
Danhoff1 <r.danhoff@st.hanze.nl>
2014-11-19 14:06:47 +0100

    Bestanden toegevoegd

---
+++ Artikel.java
@@ -0,0 +1,23 @@
+
+/**
+ * Deze klasse beschrijft een artikel
+ * 
+ * @author (Ruben Danhoff) 
+ * @version (17/11/14)
+ */
+public class Artikel
+{
+    // instance variables - replace the example below with your own
+    private String naam;
+    private int prijs;
+
+    /**
+     * Constructor for objects of class Artikel
+     */
+    public Artikel(String naam, int prijs)
+    {
+        // initialise instance variables
+       naam = naam;
+       prijs = prijs;
+    }
+}
---
+++ Persoon.java
@@ -0,0 +1,36 @@
+
+/**
+ * Die is een class die de persoon defineert
+ * 
+ * @Ruben Danhoff 
+ * @18-11-14
+ */
+
+
+public class Persoon 
+{
+    private int BSN;
+    private String voornaam;
+    private String achternaam;
+    private int geboortedag;
+    private int geboortemaand;
+    private int geboortejaar;
+    private char geslacht;
+
+public Persoon(String voornaam, String achternaam, int geboortedag, int geboortemaand, int geboortejaar, char geslacht) 
+{
+        BSN = 0;
+        voornaam = voornaam;
+        achternaam = achternaam;
+        geslacht = geslacht;   
+        geboortedag = 0; 
+        geboortemaand = 0; 
+        geboortejaar = 0; 
+    
+}
+}            
+        
+            
+            
+       
+         
